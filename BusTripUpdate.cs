e011f5a946860222bd9a189f73598ef699a09d16
Evan <devillordking@gmail.com>
2022-05-30 21:23:16 -0400

    Add Template of Cloud Function & Reader for Protocol Buffers
    Includes GTFS protocol buffers

---
+++ BusTripUpdate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Google.Protobuf;
+using TransitRealtime;
+namespace BusTripUpdate
+{
+    public class MessageBuilder
+    {
+        public MessageBuilder()
+        {
+        }
+
+        public string GetSampleMessage()
+        {
+            TripDescriptor tripDescriptor = new() { TripId = "WF_1" };
+            TripUpdate.Types.StopTimeEvent stopTimeEvent = new() { Time = 1653958416 };
+            TripUpdate tripUpdate = new() { Trip = tripDescriptor };
+            TripUpdate.Types.StopTimeUpdate stopTimeUpdate = new() { StopId = "106_171", Arrival = stopTimeEvent };
+            tripUpdate.StopTimeUpdate.Add(stopTimeUpdate);
+            FeedEntity entity = new () { Id = Guid.NewGuid().ToString(), TripUpdate = tripUpdate };
+            FeedMessage message = new();
+            message.Entity.Add(entity);
+
+
+            return EncodeMessage(message);
+        }
+
+        private string EncodeMessage(IMessage message)
+        {
+            return message.ToByteString().ToBase64();
+        }
+
+    }
+}
+
---
+++ Reader/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TransitRealtime;
+using Google.Protobuf;
+using System.Collections;
+using Google.Protobuf.Collections;
+
+namespace Reader
+{
+    class Program
+    {
+        static readonly HttpClient client = new();
+        static async Task Main()
+        {
+
+            // Call asynchronous network methods in a try/catch block to handle exceptions.
+            try
+            {
+                string test = await client.GetStringAsync("http://127.0.0.1:8080");
+
+                ByteString byteString = ByteString.FromBase64(test);
+                FeedMessage feedMessage = FeedMessage.Parser.ParseFrom(byteString);
+
+                foreach (FeedEntity entity in feedMessage.Entity)
+                {
+                    Console.WriteLine(entity.TripUpdate.Trip.TripId);
+                    Console.WriteLine(entity.TripUpdate.StopTimeUpdate.Count);
+                    foreach (TripUpdate.Types.StopTimeUpdate update in entity.TripUpdate.StopTimeUpdate)
+                    {
+                        Console.WriteLine(update.StopId);
+                        Console.WriteLine(update.Arrival.Time);
+                    }
+                }
+
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+        }
+    }
+}
+
