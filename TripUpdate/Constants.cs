68b502a577910ea74c49e8bb959d9df7434abd14
Evan <devillordking@gmail.com>
2022-06-13 14:19:42 -0400

    test build

--- TripUpdate/Constants.cs
+++ TripUpdate/Constants.cs
@@ -6,8 +6,6 @@ namespace BusTripUpdate
 		internal static string stopInfoWindwardUrl = "https://ebus.gov.vc/stopapi/webstopinfo?rid=4";
 		internal static string stopInfoLeewardUrl = "https://ebus.gov.vc/stopapi/webstopinfo?rid=5";
 
-
-
 	}
 }
 
